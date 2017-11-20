d1c583f2a652cd98c723f4f06f28db781d0128a4
JayKit <kit9870@hotmail.com>
2017-11-20 21:14:03 +0800

    Change01

---
+++ Scripts/Site.js
@@ -0,0 +1,17 @@
+﻿function DisplayClock() {
+    var momentNow = moment();
+    $("#clock").html(momentNow.format("DD MMMM YYYY (ddd), h:mm:ss a"));
+}
+
+$(document).ready(function () {
+    var interval = setInterval(function () {
+        DisplayClock();
+    }, 1000)
+
+    $(".datefield").datepicker({
+        dateFormat: "yy/mm/dd",
+        onSelect: function () {
+            $("#markSixRecordTable").DataTable().draw();
+        }
+    });
+});
